41f3ccd4a3b4bbeef51da84510147f8febcdac29
aCodingCube <lorenzwittmann2008@gmail.com>
2026-05-13 20:54:36 +0200

    added peer logic

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import {Peer} from "peerjs";
+
+console.log("P2P-TS Test :)");
+
+let peer = new Peer();
+
+peer.on("open",function(id) {
+    console.log(`My Peer id is: ${id}`);
+})
+
+let input:string|null = prompt("id:");
+
+let id:string = input == null ? "" : input;
+
+let conn = peer.connect(id);
+
+conn.on("open", function() {
+    conn.on("data", (data) => {
+        console.log(`received data: ${data}`);
+    })
+
+    conn.send(`Hello from ${id}`);
+})
